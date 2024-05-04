62b50a4ce78140f823af16a790cea84dc1ed8c86
walidwalid00 <suniskyy777@gmail.com>
2024-05-04 03:17:53 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# Guessing Game Bash Script for Unix Workbench
+
+* This Make file was ran at: 2024-05-04:03:07:54
+
+* There were 23 lines in guessinggame.sh
+
---
+++ guessinggame.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+# File: guessinggame.sh
+
+dirarray=($(ls -d */))
+dircount=${#dirarray[@]}
+dirguess=0
+
+function checkguess {
+    if [[ $1 -lt $2 ]]
+    then
+    echo "Too low"
+    elif [[ $1 -gt $2 ]]
+    then
+    echo "Too high"
+    fi
+}
+
+
+while [ $dirguess -ne $dircount ]
+do
+echo "Guess the number of directories!"
+
+read dirguess
+
+$(checkguess $dirguess $dircount)
+
+done
+echo "You got it right!  Congrats!"
